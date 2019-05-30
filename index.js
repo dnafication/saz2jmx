e5a5933bb26a24fb149cec06f329e4d6cc0da8b2
Dina <dnafication@users.noreply.github.com>
2019-05-30 18:14:53 +1000

    restructured and added __dirname

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-const sazParser = require('saz-parser');
-const _ = require('lodash');
-const { JMX } = require('./jmx');
+'use strict';
 
-const jmx = new JMX('./template.jmx');
-
-/**
- * saz2jmx
- * This program converts sazfile to jmx with good defaults
- *
- * @param {path} sazFile path to saz file
- * @param {path} exportedFile path where the file needs to be exported
- */
-function saz2jmx(sazFile, exportedFile) {
-  sazParser(sazFile, function(err, sessions) {
-    if (err) {
-      throw err;
-    }
-    // console.log(_.keys(sessions).sort())
-    _.keys(sessions)
-      .sort()
-      .forEach((key, i) => {
-        if (i < 1) {
-          // console.log(sessions[key])
-        }
-        jmx.addNewHTTPSampler(sessions[key], key);
-      });
-
-    jmx.exportAs(exportedFile);
-  });
-}
-
-// sample usage
-// saz2jmx('c:/git/20190529_SP_ConnectORderFTTN_ACTIVE.saz', 'exports/holyshit.jmx')
-
-module.exports = saz2jmx;
+module.exports = require('./lib');
